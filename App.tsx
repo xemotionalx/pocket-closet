170a446c89cbcd19f2f928735c98664c4dc3f9ca
MaiAda <maiada.carpano@croissant.com>
2023-11-25 19:41:24 -0500

    add older version of aws amplify because otherwise it doesn't work

--- App.tsx
+++ App.tsx
@@ -6,14 +6,18 @@ import "@tamagui/core/reset.css";
 import { useEffect } from "react";
 import { useCustomFonts } from "./src/hooks/useCustomFonts";
 import { LogBox } from "react-native";
+import { Amplify } from "aws-amplify";
+import amplifyconfig from "./src/amplifyconfiguration.json";
+
+Amplify.configure(amplifyconfig);
 
 SplashScreen.preventAutoHideAsync();
 
+// hide error for known issue with expo fonts from tamagui
+LogBox.ignoreLogs(['fontFamily "Inter" is not a system font']);
+
 export default function App() {
   const [customFontsLoaded] = useCustomFonts();
-
-  // hide error for known issue with expo fonts from tamagui
-  LogBox.ignoreLogs(['fontFamily "Inter" is not a system font']);
 
   useEffect(() => {
     const onLoad = async () => {
