a9c1242b07fbe017703a04e86b54b913b4ffcde8
MaiAda <maiada.carpano@croissant.com>
2023-11-25 20:48:16 -0500

    some form styling and validation

--- src/api/signUp.ts
+++ src/api/signUp.ts
@@ -1,26 +1,18 @@
 import { signUp } from "@aws-amplify/auth";
 
 type SignUpParameters = {
-  username: string;
   password: string;
   email: string;
-  phone_number: string;
 };
 
-async function handleSignUp({
-  username,
-  password,
-  email,
-  phone_number,
-}: SignUpParameters) {
+async function handleSignUp({ password, email }: SignUpParameters) {
   try {
     const { isSignUpComplete, userId, nextStep } = await signUp({
-      username,
+      username: email,
       password,
       options: {
         userAttributes: {
           email,
-          phone_number, // E.164 number convention
         },
         // optional
         autoSignIn: true, // or SignInOptions e.g { authFlowType: "USER_SRP_AUTH" }
